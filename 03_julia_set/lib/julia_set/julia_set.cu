534a1ed1252e214f7cb1451959419efb27994de1
aram-father <aram_father@naver.com>
2021-04-01 12:16:36 +0900

    style: Formatting cuda code

--- 03_julia_set/lib/julia_set/julia_set.cu
+++ 03_julia_set/lib/julia_set/julia_set.cu
@@ -16,46 +16,46 @@ __device__ Complex::Complex(const float real, const float imaginary)
 
 __device__ Complex Complex::operator*(const Complex& rhs) const
 {
-    return Complex(real_ * rhs.real_ - imaginary_ * rhs.imaginary_, real_ * rhs.imaginary_ + imaginary_ * rhs.real_);
+  return Complex(real_*rhs.real_ - imaginary_*rhs.imaginary_, real_*rhs.imaginary_ + imaginary_*rhs.real_);
 }
 
 __device__ Complex Complex::operator+(const Complex& rhs) const
 {
-    return Complex(real_ + rhs.real_, imaginary_ + rhs.imaginary_);
+  return Complex(real_+rhs.real_, imaginary_+rhs.imaginary_);
 }
 
 __device__ float Complex::GetMagnitudeSquare(void) const
 {
-    return real_ * real_ + imaginary_ * imaginary_;
+  return real_*real_ + imaginary_*imaginary_;
 }
 
 __device__ bool Complex::DoesJuliaExpansionConverge(const std::size_t length, const float threshold) const
 {
-    Complex z(this->real_, this->imaginary_);
-    Complex constant(-0.8, 0.156);
+  Complex z(this->real_, this->imaginary_);
+  Complex constant(-0.8, 0.156);
 
-    for (std::size_t it = 0; it < length; ++it)
+  for (std::size_t it = 0; it < length; ++it)
+  {
+    z = z * z + constant;
+    if (z.GetMagnitudeSquare() > threshold)
     {
-        z = z * z + constant;
-        if (z.GetMagnitudeSquare() > threshold)
-        {
-            return false;
-        }
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
 __global__ void kernel(std::uint8_t* p_image)
 {
-    float real = static_cast<float>((int)blockIdx.x - (int)gridDim.x / 2) / (gridDim.x / 2);
-    float imaginary = static_cast<float>((int)gridDim.y / 2 - (int)blockIdx.y) / (gridDim.y / 2);
+  float real = static_cast<float>((int)blockIdx.x - (int)gridDim.x / 2) / (gridDim.x / 2);
+  float imaginary = static_cast<float>((int)gridDim.y / 2 - (int)blockIdx.y) / (gridDim.y / 2);
 
-    Complex z(real, imaginary);
+  Complex z(real, imaginary);
 
-    int offset = blockIdx.y * (4 * gridDim.x) + 4 * blockIdx.x;
-    p_image[offset + 0] = z.DoesJuliaExpansionConverge() ? 255 : 0;
-    p_image[offset + 1] = 0;
-    p_image[offset + 2] = 0;
-    p_image[offset + 3] = 255;
+  int offset = blockIdx.y * (4 * gridDim.x) + 4 * blockIdx.x;
+  p_image[offset + 0] = z.DoesJuliaExpansionConverge() ? 255 : 0;
+  p_image[offset + 1] = 0;
+  p_image[offset + 2] = 0;
+  p_image[offset + 3] = 255;
 }
